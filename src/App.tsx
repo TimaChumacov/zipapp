2d8e97f7b4950fd3de759788b7e5af3c651c363d
Timophey <timchumakov@gmail.com>
2021-08-20 18:33:11 +0300

    trying to deploy to tes CROS settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const AWS = require('aws-sdk')
+
+// Configure client for use with Spaces
+const spacesEndpoint = new AWS.Endpoint('fra1.digitaloceanspaces.com');
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: '4TOXS2GRTE67RW2BB73Y',
+    secretAccessKey: 'DhQc2CHaxeE80C7VqOgghMa3a6fuk2fJHNZ8/0JuywA'
+});
+
+var params = {
+  Bucket: "chumakovz-test-1", 
+  Key: "calculator.zip"
+ };
+
+export default class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = { 
+       client: ""
+    };
+    this.test = this.test.bind(this);
+  }
+  
+  test = () => 
+  {
+    console.log("test")
+    s3.getObject(params, function(err: any, data: any) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+  }
+
+  render () 
+  {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={this.test}>test</button>
     </div>
   );
+  }
+  
 }
-
-export default App;
